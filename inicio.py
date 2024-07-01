39409ae478bf12307f06dc92e4635eec97f46ca9
jesuslopez-src <jesuslopezcm@gmail.com>
2024-07-01 15:22:24 -0400

    GUI de la pantalla de inicio aparentemente resuelta

--- inicio.py
+++ inicio.py
@@ -8,9 +8,7 @@ from screeninfo import get_monitors
 import os
 
 OUTPUT_PATH = os.path.join(os.path.dirname(__file__))
-print(OUTPUT_PATH)
 ASSETS_PATH = os.path.join(OUTPUT_PATH, "assets", "inicio", "assets", "frame0")
-print(ASSETS_PATH)
 
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
@@ -19,14 +17,12 @@ class InicioWindow(Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.monitors = get_monitors()
-        # self.geometry(f"{self.monitors[1].width}x{self.monitors[1].height}")
         self.configure(bg="#FFFFFF")
-        self.resizable(True, True)
+        self.title("El Sistema sin Nombre")
+        self.resizable(False, False)
         self.create_widgets()
 
     def create_widgets(self):
-        # background_image = PhotoImage(file=relative_to_assets("image_1.png"))
-        # imagen_mitad = background_image.subsample(2,2)
 
         imagen_raw = Image.open(str(relative_to_assets("image_1.png")))
         imagen = ImageOps.contain(imagen_raw,(self.monitors[1].width,self.monitors[1].height))
@@ -46,16 +42,6 @@ class InicioWindow(Tk):
         )
         canvas.place(x=0, y=0)
 
-        
-      
-        # width, height = imagen.size
-        # imagen = imagen.resize((int(width/2), int(height/2)))
-
-        image_image_1 = PhotoImage(file=relative_to_assets("image_1.png"))
-        # imagen_mitad = image_image_1.subsample(2,2)
-        # canvas.create_image(0, 0, image=imagen_mitad,anchor="nw")
-
-        # button_image_1 = PhotoImage(file=relative_to_assets("button_1.png"))
         button_image_1_raw = Image.open(str(relative_to_assets("button_1.png")))
         button_image_1_new_size = ImageOps.contain(button_image_1_raw,(int(self.monitors[1].width/4),int(self.monitors[1].height/5)))
         button_image_1 = ImageTk.PhotoImage(button_image_1_new_size)
@@ -67,22 +53,23 @@ class InicioWindow(Tk):
             highlightthickness=0,
             relief="flat"
         )
-        print(self.winfo_height()/2)
-        print(self.winfo_width()/2)
-        button_1.place(x=background_image.height()/2.0, y=background_image.width()/2.0, width=button_image_1.width(), height=button_image_1.height())
-        # button_1.pack()
+        print(background_image.height()/2.0)
+        print(background_image.width()/2.0)
+        button_1.place(anchor="center",x=background_image.width()/2.0, y=background_image.height()*10/11.0, width=button_image_1.width(), height=button_image_1.height())
 
-        button_image_hover_1 = PhotoImage(file=relative_to_assets("button_hover_1.png"))
+        button_hover_image_raw = Image.open(str(relative_to_assets("button_hover_1.png")))
+        button_hover_image_new_size = ImageOps.contain(button_hover_image_raw,(int(self.monitors[1].width/4),int(self.monitors[1].height/5)))
+        button_image_hover_1 = ImageTk.PhotoImage(button_hover_image_new_size)
+        
         button_1.bind('<Enter>', lambda e: button_1.config(image=button_image_hover_1))
         button_1.bind('<Leave>', lambda e: button_1.config(image=button_image_1))
 
-        image_image_2 = PhotoImage(file=relative_to_assets("image_2.png"))
-        canvas.create_image(640.0, 114.0, image=image_image_2)
+        # image_image_2 = PhotoImage(file=relative_to_assets("image_2.png"))
+        # canvas.create_image(640.0, 114.0, image=image_image_2)
         
         canvas.create_image(0, 0, anchor="nw", image=background_image)
 
-        self.image_references = [background_image, button_image_1, button_image_hover_1, image_image_2]
-
+        self.image_references = [background_image, button_image_1, button_image_hover_1]
 
     def abrir_seleccion(self):
         create_tables()
