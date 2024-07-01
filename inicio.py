53e00eb6b225560ede1c73a270fc9378619fb718
jesuslopez-src <jesuslopezcm@gmail.com>
2024-07-01 14:25:42 -0400

    primer commit sistema_biblioteca nuevo

--- inicio.py
+++ inicio.py
@@ -1,11 +1,16 @@
 from pathlib import Path
 from tkinter import Tk, Canvas, Button, PhotoImage
 from database import create_tables, insert_user
-from seleccion import SeleccionWindow  
+from seleccion import SeleccionWindow
+from PIL import ImageTk, Image, ImageOps
+from screeninfo import get_monitors
+
 import os
 
 OUTPUT_PATH = os.path.join(os.path.dirname(__file__))
+print(OUTPUT_PATH)
 ASSETS_PATH = os.path.join(OUTPUT_PATH, "assets", "inicio", "assets", "frame0")
+print(ASSETS_PATH)
 
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
@@ -13,27 +18,47 @@ def relative_to_assets(path: str) -> Path:
 class InicioWindow(Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.geometry("1280x810")
+        self.monitors = get_monitors()
+        # self.geometry(f"{self.monitors[1].width}x{self.monitors[1].height}")
         self.configure(bg="#FFFFFF")
         self.resizable(True, True)
         self.create_widgets()
 
     def create_widgets(self):
+        # background_image = PhotoImage(file=relative_to_assets("image_1.png"))
+        # imagen_mitad = background_image.subsample(2,2)
+
+        imagen_raw = Image.open(str(relative_to_assets("image_1.png")))
+        imagen = ImageOps.contain(imagen_raw,(self.monitors[1].width,self.monitors[1].height))
+        background_image = ImageTk.PhotoImage(imagen)
+        self.geometry(f"{background_image.width()}x{background_image.height()}")
+
+        print(background_image.height())
+        print(background_image.width())
         canvas = Canvas(
             self,
             bg="#FFFFFF",
-            height=810,
-            width=1280,
+            height=background_image.height(),
+            width=background_image.width(),
             bd=0,
             highlightthickness=0,
             relief="ridge"
         )
         canvas.place(x=0, y=0)
 
-        image_image_1 = PhotoImage(file=relative_to_assets("image_1.png"))
-        canvas.create_image(640.0, 407.0, image=image_image_1)
+        
+      
+        # width, height = imagen.size
+        # imagen = imagen.resize((int(width/2), int(height/2)))
 
-        button_image_1 = PhotoImage(file=relative_to_assets("button_1.png"))
+        image_image_1 = PhotoImage(file=relative_to_assets("image_1.png"))
+        # imagen_mitad = image_image_1.subsample(2,2)
+        # canvas.create_image(0, 0, image=imagen_mitad,anchor="nw")
+
+        # button_image_1 = PhotoImage(file=relative_to_assets("button_1.png"))
+        button_image_1_raw = Image.open(str(relative_to_assets("button_1.png")))
+        button_image_1_new_size = ImageOps.contain(button_image_1_raw,(int(self.monitors[1].width/4),int(self.monitors[1].height/5)))
+        button_image_1 = ImageTk.PhotoImage(button_image_1_new_size)
         button_1 = Button(
             self,
             command=self.abrir_seleccion,
@@ -42,7 +67,10 @@ class InicioWindow(Tk):
             highlightthickness=0,
             relief="flat"
         )
-        button_1.place(x=432.0, y=696.0, width=368.0, height=66.0)
+        print(self.winfo_height()/2)
+        print(self.winfo_width()/2)
+        button_1.place(x=background_image.height()/2.0, y=background_image.width()/2.0, width=button_image_1.width(), height=button_image_1.height())
+        # button_1.pack()
 
         button_image_hover_1 = PhotoImage(file=relative_to_assets("button_hover_1.png"))
         button_1.bind('<Enter>', lambda e: button_1.config(image=button_image_hover_1))
@@ -50,11 +78,11 @@ class InicioWindow(Tk):
 
         image_image_2 = PhotoImage(file=relative_to_assets("image_2.png"))
         canvas.create_image(640.0, 114.0, image=image_image_2)
-
-        background_image = PhotoImage(file=relative_to_assets("image_1.png"))
+        
         canvas.create_image(0, 0, anchor="nw", image=background_image)
 
-        self.image_references = [image_image_1, button_image_1, button_image_hover_1, image_image_2, background_image]
+        self.image_references = [background_image, button_image_1, button_image_hover_1, image_image_2]
+
 
     def abrir_seleccion(self):
         create_tables()
